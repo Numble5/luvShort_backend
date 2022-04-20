0d16bec271167eb038c1b9471a1c20cee6d608f1
savannah030 <savannah030@naver.com>
2022-04-21 03:02:33 +0900

    feat : #2 add `findByEmail` function in UserRepository

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.backend.repository;
 import com.example.backend.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 이미 가입한 사용자인지 확인할 때 이메일을 이용
+    Optional<User> findByEmail(String email);
 }
