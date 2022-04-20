5c8cac172495654540e0f0373e01a3511c2d48f4
savannah030 <savannah030@naver.com>
2022-04-21 03:39:54 +0900

    feat : #3 create `ReturnCode` enum used in exception handling

---
+++ src/main/java/com/example/backend/exception/ReturnCode.java
@@ -0,0 +1,13 @@
+package com.example.backend.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReturnCode {
+
+    USER_EXIST_WITH_EMAIL(400, "해당 이메일로 이미 가입한 계정이 존재합니다.");
+    private final int status;
+    private final String message;
+}
