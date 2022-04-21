4cc9093132516ff7d024883395b63bbccf215aa5
savannah030 <savannah030@naver.com>
2022-04-21 18:12:15 +0900

    rename : #1 change file name WebConfig to WebMvcConfig

--- src/main/java/com/example/backend/config/WebMvcConfig.java
+++ src/main/java/com/example/backend/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class WebConfig {
+public class WebMvcConfig {
     // HTTP 통신할 때 쓸 객체 만들어주는 간단한 템플릿 메서드
     @Bean
     public RestTemplate restTemplate(){
