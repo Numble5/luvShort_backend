dcfd4ba8382d9551d6ac45d8b6b455d401f78d74
savannah030 <savannah030@naver.com>
2022-04-22 02:05:53 +0900

    feat : #1 create SignInDto

---
+++ src/main/java/com/example/backend/dto/SignInDto.java
@@ -0,0 +1,16 @@
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInDto {
+    private String token;
+    private String email;
+    private String nickname;
+}
