0eb2908a05fc65982a7844276a71c3adee97a9c3
savannah030 <savannah030@naver.com>
2022-04-21 16:13:08 +0900

    fix : #3 fix KakaoAccessTokenResponseDto

--- src/main/java/com/example/backend/config/dto/KakaoAccessTokenResponseDto.java
+++ src/main/java/com/example/backend/config/dto/KakaoAccessTokenResponseDto.java
@@ -3,20 +3,31 @@ package com.example.backend.config.dto;
 import com.example.backend.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import net.minidev.json.JSONObject;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Getter
 public class KakaoAccessTokenResponseDto {
-    // FIXME: 카카오 사용자 정보 어떤 형태로 주는지에 따라 코드 바꿔야 할수도
     // 카카오 REST API 공식문서 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
-    private Map<String, Object> attributes;
-    private String email;
+    private final Long id;
+    private final Boolean has_signed_up;
+    private final LocalDateTime connected_at; //FIXME: UTC to KST
+    private final LocalDateTime synched_at;
+    private final JSONObject properties;
+    private final JSONObject KakaoAccount;
+
+    private final String email = "example@example.com"; // FIXME
 
     @Builder
-    public KakaoAccessTokenResponseDto(Map<String, Object> attributes, String email) {
-        this.attributes = attributes;
-        this.email = email;
+    public KakaoAccessTokenResponseDto(Long id, Boolean has_signed_up, LocalDateTime connected_at, LocalDateTime synched_at, JSONObject properties, JSONObject KakaoAccount) {
+        this.id = id;
+        this.has_signed_up = has_signed_up;
+        this.connected_at = connected_at;
+        this.synched_at = synched_at;
+        this.properties = properties;
+        this.KakaoAccount = KakaoAccount;
     }
 
     public User toEntity(){
