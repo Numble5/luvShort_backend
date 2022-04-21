b06fcef3a1f7a938c729fc6cf527fb278b320284
savannah030 <savannah030@naver.com>
2022-04-22 02:05:11 +0900

    feat : #4 create ResponseDto used for error response

---
+++ src/main/java/com/example/backend/dto/ResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.backend.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO {
+    private String error;
+    private List<?> data;
+}
