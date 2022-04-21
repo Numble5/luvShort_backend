183f9f45867e0dd605218cb54ecab9df23a4ae69
savannah030 <savannah030@naver.com>
2022-04-21 17:25:10 +0900

    feat : #1 create KakaoApiController

---
+++ src/main/java/com/example/backend/controller/api/KakaoApiController.java
@@ -0,0 +1,34 @@
+package com.example.backend.controller.api;
+
+import com.example.backend.config.dto.KakaoAccessToken;
+import com.example.backend.service.api.KakaoApiService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth/kakao-login")
+public class KakaoApiController {
+
+    private final KakaoApiService kakaoApiService;
+    @PostMapping
+    public ResponseEntity<?> test(@RequestBody KakaoAccessToken kakaoAccessToken){
+        log.info("KakaoAccessToken: {}",kakaoAccessToken);
+        log.info("access_token : {}",kakaoAccessToken.getAccess_token());
+        log.info("expires_in : {}",kakaoAccessToken.getExpires_in());
+        log.info("refresh_token : {}",kakaoAccessToken.getRefresh_token());
+        log.info("refresh_token_expires_in : {}",kakaoAccessToken.getRefresh_token_expires_in());
+        log.info("scope : {}",kakaoAccessToken.getScope());
+        log.info("token_type : {}",kakaoAccessToken.getToken_type());
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+
+    }
+
+}
