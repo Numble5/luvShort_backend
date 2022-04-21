f9860d510b9e91fee069887c5ed028a6f1ea5928
savannah030 <savannah030@naver.com>
2022-04-21 13:48:44 +0900

    rename : #1 change file name OAuthAttributesDto to KakaoAccessTokenResponseDto

--- src/main/java/com/example/backend/config/dto/KakaoAccessTokenResponseDto.java
+++ src/main/java/com/example/backend/config/dto/KakaoAccessTokenResponseDto.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 import java.util.Map;
 
 @Getter
-public class OAuthAttributesDto {
+public class KakaoAccessTokenResponseDto {
     // FIXME: 카카오 사용자 정보 어떤 형태로 주는지에 따라 코드 바꿔야 할수도
+    // 카카오 REST API 공식문서 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
     private Map<String, Object> attributes;
     private String email;
 
     @Builder
-    public OAuthAttributesDto(Map<String, Object> attributes, String email) {
+    public KakaoAccessTokenResponseDto(Map<String, Object> attributes, String email) {
         this.attributes = attributes;
         this.email = email;
     }
--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -1,15 +1,12 @@
 package com.example.backend.service;
 
-import com.example.backend.config.dto.OAuthAttributesDto;
-import com.example.backend.domain.user.User;
+import com.example.backend.config.dto.KakaoAccessTokenResponseDto;
 import com.example.backend.exception.BackendException;
 import com.example.backend.exception.ReturnCode;
 import com.example.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,9 +15,9 @@ public class UserService {
     private final UserRepository userRepository;
 
     // 인증 서버(카카오, 네이버...)에서 받은 정보로 User 만들기
-    public void createUser(OAuthAttributesDto oAuthAttributesDto){
-        final String email = oAuthAttributesDto.getEmail();
-        log.info("email from oAuthAttributesDto : {}",email);
+    public void createUser(KakaoAccessTokenResponseDto kakaoAccessTokenResponseDto){
+        final String email = kakaoAccessTokenResponseDto.getEmail();
+        log.info("email from kakaoAccessTokenResponseDto : {}",email);
 
         // 해당 이메일이 이미 User 테이블에 있으면 Exception 처리
         if(userRepository.existsByEmail(email)){
@@ -28,7 +25,7 @@ public class UserService {
             throw new BackendException(ReturnCode.USER_EXIST_USING_THIS_EMAIL);
         }
         // 해당 이메일이 User 테이블에 없을 때만 User 엔티티 만들기
-        userRepository.save(oAuthAttributesDto.toEntity());
+        userRepository.save(kakaoAccessTokenResponseDto.toEntity());
         log.info("user saved");
     }
 }
