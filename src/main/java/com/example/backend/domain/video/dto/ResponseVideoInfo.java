2a5cf34a063e441250ba9c6a677024473409e3d0
juya <kjy2967@naver.com>
2022-04-24 03:42:12 +0900

    [backend / feat] #10 CREATE Get all video api (no category) controller,service,dto

---
+++ src/main/java/com/example/backend/domain/video/dto/ResponseVideoInfo.java
@@ -0,0 +1,47 @@
+package com.example.backend.domain.video.dto;
+
+import com.example.backend.domain.video.Video;
+import com.example.backend.domain.video.enums.VideoType;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDateTime;
+
+@Getter
+public class ResponseVideoInfo {
+    // 비디오 정보
+    private Long video_idx;
+    private VideoType videoType;
+
+    private String title;
+    private String content;
+    private Long hits;
+    private String thumbnailUrl;
+    private String videoUrl;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    // 사용자 정보
+    private String nickname;
+    private String profileImgUrl;
+
+    @Builder
+    public ResponseVideoInfo(Long idx, VideoType videoType, String title, String content, Long hits, String thumbnailUrl, String videoUrl, String nickname, String profileImgUrl,LocalDateTime createdDate,LocalDateTime updatedDate) {
+        this.video_idx = idx;
+        this.videoType = videoType;
+        this.title = title;
+        this.content = content;
+        this.hits = hits;
+        this.thumbnailUrl = thumbnailUrl;
+        this.videoUrl = videoUrl;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.nickname = nickname;
+        this.profileImgUrl = profileImgUrl;
+    }
+
+}
