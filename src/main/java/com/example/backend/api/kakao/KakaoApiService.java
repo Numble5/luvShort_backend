d3d05d757298f5163aac63c214e6233475977545
savannah030 <savannah030@naver.com>
2022-04-23 15:15:23 +0900

    fix: #6 remove redundant codes

--- src/main/java/com/example/backend/api/kakao/KakaoApiService.java
+++ src/main/java/com/example/backend/api/kakao/KakaoApiService.java
@@ -27,7 +27,7 @@ public class KakaoApiService {
     private final ObjectMapper objectMapper;
 
 
-    // 방법 1 :카카오 '사용자 정보 가져오기' API에서 accessToken으로 정보 받아오기
+    // 카카오 '사용자 정보 가져오기' API에서 accessToken으로 정보 받아오기
     // cf) 카카오 '사용자 정보 가져오기' API https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
     /**
      * // 요청
@@ -75,54 +75,4 @@ public class KakaoApiService {
             throw new BackendException(ReturnCode.FAIL_TO_GET_KAKAO_ACCOUNT);
         }
     }
-
-    // 방법 2: 카카오 '사용자 정보 가져오기' API에서 어드민키로 정보 받아오기
-    // https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info-admin-key
-    public KakaoUserInfoResponseDto getKakaoAccount(String accessToken){
-
-        // 요청 관련 API 문서 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info-request
-        // 응답 관련 API 문서 https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info-response
-
-        /**
-         * 요청
-         */
-        // 1. Header
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        // 2. Parameter
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        //params.add("target_id_type",targetIdType); //string 회원번호 종류, user_id로 고정
-        //params.add("target_id", String.valueOf(targetId));//long 사용자 정보를 가져올 사용자의 회원번호
-
-        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(params,headers);
-
-
-        /**
-         * 응답
-         */
-        String url = "https://kapi.kakao.com/v2/user/me";
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-            KakaoUserInfoResponseDto kakaoUserInfoResponseDto = objectMapper.readValue(response.getBody(), KakaoUserInfoResponseDto.class);
-
-            /*
-            // 타입 출력
-            try{
-                log.info("KakaoApiService response: {}",response.toString());
-            } catch (NullPointerException ex) {
-                ex.printStackTrace();
-                throw new Exception();
-            }
-            //return objectMapper.readValue(kakaoUserInfoResponseDto, KakaoUserInfoResponseDto.class);
-            return response;
-             */
-            return kakaoUserInfoResponseDto;
-        } catch (Exception ex){
-            ex.printStackTrace();
-            throw new BackendException(ReturnCode.FAIL_TO_GET_KAKAO_ACCOUNT);
-        }
-
-    }
-
 }
--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -59,52 +59,6 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody KakaoAccessToken requestDto) throws BackendException {
-        log.info("KakaoAccessToken: {}",requestDto);
-        String accessToken = requestDto.getAccessToken();
-        // step3 사용자 정보 가져오기
-        KakaoUserInfoResponseDto dto = kakaoApiService.getKakaoAccount(accessToken);
-        log.info("id : {}", dto.getId());
-        log.info("has_signed_up : {}", dto.getHas_signed_up());
-        log.info("connected_at : {}", dto.getConnected_at());
-        log.info("synched_at : {}", dto.getSynched_at());
-        log.info("properties : {}", dto.getProperties());
-        log.info("KakaoAccount : {}", dto.getKakaoAccount());
-
-        /**
-         * HTTP/1.1 200 OK
-         * {
-         *     "id":123456789,
-         *     "kakao_account": {
-         *         "profile_needs_agreement": false,
-         *         "profile": {
-         *             "nickname": "홍길동",
-         *             "thumbnail_image_url": "http://yyy.kakao.com/.../img_110x110.jpg",
-         *             "profile_image_url": "http://yyy.kakao.com/dn/.../img_640x640.jpg",
-         *             "is_default_image":false
-         *         },
-         *         "name_needs_agreement":false,
-         *         "name":"홍길동",
-         *         "email_needs_agreement":false,
-         *         "is_email_valid": true,
-         *         "is_email_verified": true,
-         *         "email": "sample@sample.com",
-         *         "age_range_needs_agreement":false,
-         *         "age_range":"20~29",
-         *         "birthday_needs_agreement":false,
-         *         "birthday":"1130",
-         *         "gender_needs_agreement":false,
-         *         "gender":"female"
-         *     },
-         *     "properties":{
-         *         "nickname":"홍길동카톡",
-         *         "thumbnail_image":"http://xxx.kakao.co.kr/.../aaa.jpg",
-         *         "profile_image":"http://xxx.kakao.co.kr/.../bbb.jpg",
-         *         "custom_field1":"23",
-         *         "custom_field2":"여"
-         *         ...
-         *     }
-         * }
-         */
 
         return new ResponseEntity<>("{}", HttpStatus.OK);
 
