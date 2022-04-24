28077a937f8a6ac9c84ebc50f6043d08287aeaf1
savannah030 <savannah030@naver.com>
2022-04-24 15:22:54 +0900

    fix: #4 merge WebSecurityConfig

--- src/main/java/com/example/backend/config/WebSecurityConfig.java
+++ src/main/java/com/example/backend/config/WebSecurityConfig.java
@@ -5,15 +5,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@RequiredArgsConstructor
 @EnableWebSecurity
-@Configuration
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+                .and()
+                .csrf()// csrf는 현재 사용하지 않으므로 disable
+                .disable()
+                .httpBasic()// token을 사용하므로 basic 인증 disable
+                .disable()
+                .sessionManagement()  // session 기반이 아님을 선언
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers("**/api/videos/**")
-                .permitAll();
+                .antMatchers("/", "/api/auth/kakao-login/**", "**/api/videos/**").permitAll(); // /와 /api/auth/kakao-login/** 경로는 인증 안해도 됨.
+        //.anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+        //.authenticated()
+
+
     }
 }
