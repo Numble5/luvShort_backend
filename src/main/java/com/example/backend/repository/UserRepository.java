baaa6e0fcf04ead5711aeb87c609551a0f72187f
savannah030 <savannah030@naver.com>
2022-04-26 22:12:56 +0900

    feat: #29 update UserRepository, UserInterestRepository, InterestRepository

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -2,10 +2,8 @@ package com.example.backend.repository;
 
 import com.example.backend.domain.user.User;
 import com.example.backend.domain.user.enums.SocialAccountType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -13,8 +11,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 이미 가입한 사용자인지 확인할 때 이메일을 이용
     Optional<User> findByEmail(String email);
 
-    // 해당 이메일이 User 엔티티에 있는지 확인
+    @Query("select (count(u) > 0) from User u where u.email = ?1")
     Boolean existsByEmail(String email);
+
+    // 해당 이메일을 갖는 User 엔티티가 유일한지 확인
+    @Query("select (count(u) = 1) from User u where u.email = ?1")
+    Boolean existsOnlyByEmail(String email);
 
     Boolean existsByNickname(String nickname);
 
