e07d8fc19beddc268e1340193ad63a6c80f4fd75
savannah030 <savannah030@naver.com>
2022-04-26 17:23:02 +0900

    fix: #2 fix InterestName to interestName and add Builder pattern

--- src/main/java/com/example/backend/domain/user/Interest.java
+++ src/main/java/com/example/backend/domain/user/Interest.java
@@ -1,6 +1,10 @@
 package com.example.backend.domain.user;
 
 import com.example.backend.domain.BaseEntity;
+import com.example.backend.domain.user.embedded.UserInfo;
+import com.example.backend.domain.user.enums.RoleType;
+import com.example.backend.domain.user.enums.UserStatus;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,5 +20,11 @@ public class Interest extends BaseEntity {
     @Column(name = "interest_idx")
     private Long idx;
 
-    private String InterestName;
+    private String interestName;
+
+    @Builder
+    public Interest(String interestName){
+        this.interestName = interestName;
+    }
+
 }
