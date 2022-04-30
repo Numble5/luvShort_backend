585e28e94bf1b150bc26d84e1bb008718bcc5c04
juya <kjy2967@naver.com>
2022-05-01 05:00:18 +0900

    [backend / feat]: CREATE video pagination api sort by id desc

--- src/main/java/com/example/backend/repository/VideoRepository.java
+++ src/main/java/com/example/backend/repository/VideoRepository.java
@@ -1,8 +1,16 @@
 package com.example.backend.repository;
 
 import com.example.backend.domain.video.Video;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    List<Video> findAllBy();
+    Page<Video> findByIdxLessThan(Long lastId, Pageable page);
+    Video findTop1ByOrderByIdxDesc();
 
 }
