faa9fb80fcf5dd4d0503210222035cc30125086c
savannah030 <savannah030@naver.com>
2022-05-11 01:00:14 +0900

    refactor: #1 fix getUserInfoByJwt function in UserService

--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -106,9 +106,12 @@ public class UserService {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         Optional<User> user = userRepository.findByEmail(email);
-        // 엔티티 객체 없으면 있으면 유저정보 리턴
-        // 없으면 null 리턴
-        if (user.isPresent()){
+        // 엔티티 객체 없으면 null 리턴
+        if (!user.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        // 엔티티 객체 있으면 유저정보 리턴
+        else{
             List<UserInterest> userInterests = userInterestRepository.findUserInterestsByUser(user.get());
             List<String> interestStr = new LinkedList<>();
             for(UserInterest userInterest: userInterests){
@@ -116,8 +119,7 @@ public class UserService {
             }
             return new ResponseEntity<>(new UserReponseDtoByCookie(user.get(),interestStr), HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
+    }
 
 
     @Transactional
