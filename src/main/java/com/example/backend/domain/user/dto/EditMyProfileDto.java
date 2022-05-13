f012aa1bd80d92a5e95625d7374e07f34f5160c8
savannah030 <savannah030@naver.com>
2022-05-14 02:46:14 +0900

    fix: #111 fix EditMyProfileDto

--- src/main/java/com/example/backend/domain/user/dto/EditMyProfileDto.java
+++ src/main/java/com/example/backend/domain/user/dto/EditMyProfileDto.java
@@ -1,5 +1,6 @@
 package com.example.backend.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class EditMyProfileDto {
 
     private String profileImg;
