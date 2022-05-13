b39ba1b8ff2b8b0009bf26d0a73b0a13c68857cb
savannah030 <savannah030@naver.com>
2022-05-13 15:23:49 +0900

    fix: #1 add roleType in SignUpResponseDto

--- src/main/java/com/example/backend/domain/user/dto/SignUpResponseDto.java
+++ src/main/java/com/example/backend/domain/user/dto/SignUpResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.backend.domain.user.dto;
 
 import com.example.backend.domain.user.User;
+import com.example.backend.domain.user.enums.RoleType;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,12 +11,14 @@ public class SignUpResponseDto {
     private Long idx;
     private String nickname;
     private String email;
+    private RoleType roleType;
 
     @Builder
     public SignUpResponseDto(User entity){
         this.idx = entity.getIdx();
         this.nickname = entity.getNickname();
         this.email = entity.getEmail();
+        this.roleType = entity.getRoleType();
     }
 
 }
